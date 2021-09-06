00f9944498961c5fd790248ea34401b4e1e623a7
Dan <danielmcollins1996@gmail.com>
2021-09-06 11:23:14 -0600

    Running Server

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-const { gql } = require('apollo-server');
+const { ApolloServer, gql } = require('apollo-server');
 
 const typeDefs = gql`
     type Query {
         greeting: String
     }
 `
+// Resolver must mirror query object exactly
+const resolvers = {
+    Query: {
+        greeting: () => 'Hello GraphQL world!'
+    }
+}
 
-console.log(typeDefs);
+const server = new ApolloServer({typeDefs, resolvers});
+server.listen({port: 9000})
+      .then((serverInfo) => console.log(serverInfo))
